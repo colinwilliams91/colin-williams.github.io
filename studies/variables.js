44d878e3a083cef1adadf9aab5e04f4655659cdb
colinwilliams91 <colin.williams91@gmail.com>
2022-08-04 19:24:23 +0000

    started studies project

---
+++ studies/variables.js
@@ -0,0 +1,15 @@
+/**
+ * VARIABLES
+ * 
+ * 0: Variables are named containers for data in javascript. They can hold...
+ * 
+ * 1: Declaration and assignment: Variables are declared with a declaritive keyword
+ *
+*/
+
+// Declaration
+// newVar variable
+var newVar;
+
+// Assignment
+newVar = "new variable!";
