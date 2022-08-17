ba2fdc72736f6ac1143ba8c6b09f80fa22c86c9b
colinwilliams91 <colin.williams91@gmail.com>
2022-08-17 03:03:30 +0000

    functions studies started

---
+++ studies/functions.js
@@ -0,0 +1,38 @@
+
+/**
+ * FUNCTIONS
+ * 
+ * 0: A Function is a reusable piece of code. It can be defined after the keyword "function".
+ * 
+ * 1: A Function will sometimes expect parameters (function(para1, para2)).
+ * these can be any data-type including other functions.
+ * 
+ * 2: What we pass into these parameters at the time of invocation (or the "call")
+ * are called arguments, and will be used inside the function to determine the return.
+ * 
+ * 3: After these (parameters) we will {write a code block inside curly braces}
+ * which will be executed whenever we call the function.
+ * 
+ * 4:
+ * 
+ * 
+ * 
+ */
+
+// FUNCTION DEFINITION / DECLARATION
+
+
+
+
+
+// FUNCTION CALL / INVOCATION
+
+
+
+
+
+// FUNCTION ARGUMENTS
+
+/**
+ * arguments passed to a function are actually an object that can be iterated through or accessed. ex: arguments[i], arguments.length
+ */
