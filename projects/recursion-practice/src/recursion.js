a6bef65427b1f9ce7a1c5619736e5bc260ef6116
colinwilliams91 <colin.williams91@gmail.com>
2022-08-23 03:57:12 +0000

    finished isEven and sumBelow

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -31,12 +31,29 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === -1 || n === 1){
+    return false;
+  } else if (n === 0){
+    return true;
+  } else if (n > 0){
+    return isEven(n - 2);
+  } else if (n < 0){
+    return isEven(n + 2);
+  }
+  return n;
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n > 0){
+    return n - 1 + sumBelow(n - 1);
+  } else if (n < 0){
+    return n + 1 + sumBelow(n + 1);
+  } else if (n === 0){
+    return n;
+  }
 };
 
 // 6. Get the integers in range (x, y).
