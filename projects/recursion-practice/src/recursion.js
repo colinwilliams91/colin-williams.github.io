4e9f1a428a1b34b15e1c75b38a6ed51d55d78b40
colinwilliams91 <colin.williams91@gmail.com>
2022-08-24 01:56:49 +0000

    finished powerOfTwo function

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -76,6 +76,13 @@ var range = function(x, y, output=[]) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0){
+    return 1;
+  } else if (exp > 0){
+    return base * exponent(base, exp - 1);
+  } else if (exp < 0){
+    return 1 / exponent(base, -exp);
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -83,9 +90,16 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1){
+    return true;
+  } else if (n > 1){
+    return powerOfTwo(n / 2);
+  } else {
+    return false;
+  }
 };
 
-// 9. Write a function that accepts a string a reverses it.
+// 9. Write a function that accepts a string and reverses it.
 var reverse = function(string) {
 };
 
